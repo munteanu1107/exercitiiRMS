1710f8a589460e71e6eab98a2f2bd7c116494549
Mihai Munteanu <mum@ad.rms.intern>
2018-05-15 11:51:16 +0300

    EX05-15 Rezolvat exercitiu prin care verificam ca un numar este prim

---
+++ verificare_numar_prim/verif_numar_prim.js
@@ -0,0 +1,24 @@
+    var executeVerification = document.getElementById("js-verifica-numar");
+
+    function verifyNumber() {
+        var insertedNumber = document.getElementById("primeNumber").value;
+
+        if((insertedNumber - 0) === 1 || (insertedNumber - 0) === 2) {
+            document.getElementsByClassName("result")[0].innerHTML = "Numarul " + insertedNumber + " este prim";
+
+            console.log("Numarul " + insertedNumber + " este prim")
+        }
+
+        for(var i = 2; i < insertedNumber; ++i) {
+            if(insertedNumber % i === 0) {
+                console.log("Numarul " + insertedNumber + " nu este prim");
+
+                return document.getElementsByClassName("result")[0].innerHTML = "Numarul " + insertedNumber + " nu este prim";
+            }
+            console.log("Numarul " + insertedNumber + " este prim")
+
+            return document.getElementsByClassName("result")[0].innerHTML = "Numarul " + insertedNumber + " este prim";
+        }
+    }
+
+    executeVerification.addEventListener("click", verifyNumber);
