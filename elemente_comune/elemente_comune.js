83d48a2b5c5402a5fde3212384e8387126f42c5c
Mihai Munteanu <mum@ad.rms.intern>
2018-05-24 07:52:59 +0300

    Refacut exercitiu elemente comune si rezolvat exercitiu palindroame in vectori

--- elemente_comune/elemente_comune.js
+++ elemente_comune/elemente_comune.js
@@ -3,35 +3,63 @@ var executeVerification = document.getElementById("js-verifica-numar");
     // Verifica in console
 
 function findCommonElements() {
-    var arrayOne = [2, 3, 1, 4, 7, 5, 6, 8];
+    var arrayOne = [2, 3, 1, 4, 7, 5, 6, 8, 30, 30];
     var arrayTwo = [11, 5, 8, 12, 9, 15, 1];
     var commonElements = [];
     var hashMap = {};
 
-    maxL = Math.max(arrayOne.length, arrayTwo.length);
+    // for(var i = 0; i < arrayOne.length; i++) {
+    //     for(var j = 0; j < arrayTwo.length; j++) {
+    //         if(arrayOne[i] === arrayTwo[j]) {
+    //             commonElements[commonElements.length] = arrayOne[i]
+    //         }
+    //     }
+    // }
 
-    for(var i = 0; i < maxL; i++) {
-
-        if (i < arrayOne.length) {
-            if(hashMap[arrayOne[i]]) {
-                hashMap[arrayOne[i]] += 1;
-            } else {
-                hashMap[arrayOne[i]] = 1;
+    for(var i = 0; i < arrayOne.length; i++) {
+        if (hashMap[arrayOne[i]]) {
+            hashMap[arrayOne[i]].count++;
+            if (hashMap[arrayOne[i]].v.indexOf("A") == -1) {
+                hashMap[arrayOne[i]].v += "A";
             }
+        } else {
+            hashMap[arrayOne[i]] = {};
+            hashMap[arrayOne[i]].count = 1;
+            hashMap[arrayOne[i]].v = "A";
         }
 
-        if (i < arrayTwo.length) {
-            if(hashMap[arrayTwo[i]]) {
-                hashMap[arrayTwo[i]] += 1;
-            } else {
-                hashMap[arrayTwo[i]] = 1;
+    }
+
+    for(var i = 0; i < arrayTwo.length; i++) {
+        if (hashMap[arrayTwo[i]]) {
+            hashMap[arrayTwo[i]].count++;
+            if (hashMap[arrayTwo[i]].v.indexOf("B") == -1) {
+                hashMap[arrayTwo[i]].v += "B";
             }
+        } else {
+            hashMap[arrayTwo[i]] = {};
+            hashMap[arrayTwo[i]].count = 1;
+            hashMap[arrayTwo[i]].v = "B";
         }
 
     }
 
     for(var key in hashMap) {
-        hashMap[key] > 1 ? commonElements[commonElements.length] = parseInt(key) : commonElements;
+        if (hashMap[key].count > 1 &&  hashMap[key].v.indexOf("A") != -1 && hashMap[key].v.indexOf("B") != -1){
+            commonElements.push(key);
+        }
+
+        if(hashMap[key].v.indexOf("B") == -1) { // care sunt in A si nu sunt in B
+            // console.log(key)
+        }
+
+        if(hashMap[key].v.indexOf("A") == -1) { // care sunt in B si nu in A
+            // console.log(key)
+        }
+
+        if(hashMap[key].v.indexOf("A") > -1 && hashMap[key].v.indexOf("B") > -1) { //
+            console.log(key)
+        }
     }
 
     console.log(arrayOne);
