8e298b9f52ca73af0e54d78f04dcb786b5a908db
Mihai Munteanu <mum@ad.rms.intern>
2018-06-18 07:37:25 +0300

    Rezolvare exercitiu generare patrate

--- generare_patrate/generare_patrate.js
+++ generare_patrate/generare_patrate.js
@@ -1,52 +1,90 @@
 window.onload = (function() {
     var container;
     var row;
-    var parentSquare;
     var square;
+    var rowXPos;
 
-    var initialVal = 4;
-    var test = initialVal;
+    var startVal = 4;
+    var flag = true;
+    var squareHeight = 50;
+    var squareWidth = squareHeight;
+    var distance = 4;
+    var xPos = 4;
+    var yPos = 4;
 
     container = document.getElementById("container");
-    square = document.createElement("div");
+    yPos = (distance * (yPos + squareHeight)) - squareHeight;
+    rowXPos = 4;
 
-    function createSquare() {
+    function createSquare(row) {
         square = document.createElement("div");
-        square.style.width = "50px";
-        square.style.height = "50px";
-        square.style.margin = "5px";
-        square.style.backgroundColor = "green";
-        square.style.display = "inline-block";
+        square.style.width = squareWidth + "px";
+        square.style.height = squareHeight + "px";
+        square.style.backgroundColor = "red";
+        square.style.position = "absolute";
+        square.style.left = xPos + "px"
 
-        parentSquare.appendChild(square);
+        xPos += (distance + squareWidth);
+        row.style.position = "absolute";
+        row.style.top = yPos + "px";
+        row.style.left = rowXPos + "px";
+        row.appendChild(square);
     }
 
-    function createRows(num) {
-        if(num === 0) {
+    function recursiveGenerateSquares(no) {
+
+        if (yPos < 0) {
+            yPos = ((distance) * (distance + squareHeight)) + distance;
+             rowXPos = (distance + squareWidth) / 2;
+        }
+
+        row = document.createElement("div");
+
+        if(no === 0) {
+            if(flag) {
+                flag = false;
+                recursiveGenerateSquares(startVal - 1)
+            }
             return;
         }
 
-        parentSquare = document.createElement("div");
+        if(flag) {
+            createNElements(no);
+        } else{
+            createNElements1(no);
+        }
 
-        createSquaresPerRows(num);
-
-        return createRows(num - 1);
+        return recursiveGenerateSquares(no - 1);
     }
 
-    function createSquaresPerRows(num) {
-        if(num === 0) {
-            row = document.createElement("div");
-            row.appendChild(parentSquare);
+    function createNElements(number) {
+        if(number === 0) {
+            xPos = distance;
+            yPos -= (distance + squareWidth);
+            rowXPos += squareWidth / 2;
             container.appendChild(row);
-
             return;
-
-        } else {
-            createSquare();
         }
 
-        return createSquaresPerRows(num - 1);
+        createSquare(row,xPos);
+
+        return createNElements(number - 1);
     }
 
-    createRows(initialVal)
+    function createNElements1(number) {
+
+        if (number === 0) {
+            xPos = distance;
+            yPos += (distance + squareWidth);
+            rowXPos += (distance + squareWidth) / 2;
+            container.appendChild(row);
+            return;
+        }
+
+        createSquare(row, xPos);
+
+        return createNElements1(number - 1);
+    }
+
+    recursiveGenerateSquares(startVal);
 }());
