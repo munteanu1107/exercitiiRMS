754961adc64dcd87d92126fd411a59e1ac244b84
Mihai Munteanu <mum@ad.rms.intern>
2018-06-19 18:52:46 +0300

    Arbore binar still in progress

---
+++ arbore_binar/arbore_binar.js
@@ -0,0 +1,18 @@
+window.onload = (function() {
+    var container;
+    var controller;
+    var tree;
+    var depth;
+    var length;
+    var scaleLength;
+    var theta;
+
+    container = document.getElementById("container");
+    controller = document.getElementById("controller");
+    tree = document.getElementById("tree");
+
+    depth = document.getElementById("depth").value;
+    length = document.getElementById("length").value;
+    scaleLength = document.getElementById("lengthScale").value;
+    theta = document.getElementById("dTheta").value;
+}());
