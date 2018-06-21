9d1785101bb684d4d9d368611bbb22446f81520d
Mihai Munteanu <mum@ad.rms.intern>
2018-06-21 08:44:49 +0300

    rezolvare arbore binar

--- arbore_binar/arbore_binar.js
+++ arbore_binar/arbore_binar.js
@@ -20,13 +20,13 @@ window.onload = (function() {
     canvas.height = 600;
     var ctx = canvas.getContext('2d');
 
-
-function DrawBranch(depth, x, y, length, theta, length_scale, dtheta, height) {
+    function DrawBranch(depth, height, x, y, length, theta, length_scale, dtheta) {
         ctx.beginPath();
+
         var x1 = 0;
         var y1 = 0;
 
-        if(depth === height) {
+        if (depth == height) {
             ctx.moveTo(x, y);
             ctx.lineTo(x, y - length);
             ctx.stroke();
@@ -34,22 +34,35 @@ function DrawBranch(depth, x, y, length, theta, length_scale, dtheta, height) {
             x1 = x;
             y1 = y - length;
         } else {
-
-
             ctx.moveTo(x, y);
-            x1 = (x - length * Math.cos(theta * (Math.PI / 180)));
-            y1 = (y - length * Math.sin(theta * (Math.PI / 180)));
+
+            // See where this branch should end.
+            x1 = (x + length * Math.cos((theta) * Math.PI / 180));
+            y1 = (y - length * Math.sin((theta) * Math.PI / 180));
 
             ctx.lineTo(x1, y1);
             ctx.stroke();
         }
 
-    if (depth > 1) {
-        DrawBranch(depth - 1, x1, y1, length * length_scale, theta + dtheta, length_scale, dtheta, height);
-        DrawBranch(depth - 1, x1, y1, length * length_scale, theta - dtheta, length_scale, dtheta, height);
-    }
-}
+        // If depth > 1, draw the attached branches.
+        if (depth > 1) {
+            DrawBranch(depth - 1, height, x1, y1, length * length_scale, theta + dtheta, length_scale, dtheta);
+            DrawBranch(depth - 1, height, x1, y1, length * length_scale, theta - dtheta, length_scale, dtheta);
+        }
 
-    DrawBranch(10, 250, 300, 50, 45, 0.75, 35, 10);
+    }
+    var contor = 0;
+    var step = 1;
+
+    var t = setInterval(function() {
+        ctx.clearRect(0,0,600,600);
+        ctx.fill()
+        DrawBranch(16, 16, 250, 300, 70, 90, 0.75, contor);
+        contor+= step;
+        if (contor < 0 || contor > 90) {
+            step *= -1;
+        }
+    }, 100)
+
 
 }());
