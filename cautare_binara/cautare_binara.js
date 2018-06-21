3cdcbbdfd744d5b887921767509266156c78a0bb
Mihai Munteanu <mum@ad.rms.intern>
2018-06-21 09:51:38 +0300

    rezolvare exercitiu cautare binara

--- cautare_binara/cautare_binara.js
+++ cautare_binara/cautare_binara.js
@@ -1,3 +1,27 @@
 window.onload = (function() {
+    var array = [1, 5, 6, 8, 9, 23, 26, 48, 56, 57, 58, 63, 70];
 
+    function binarySearch(num, arr, newArray) {
+        var halfPosition = Math.floor((arr.length - 1) / 2);
+        var newArray;
+
+
+        if(num === arr[halfPosition]) {
+            return console.log("Found " + arr[halfPosition] + " in given array!");
+
+        } else if(num > arr[halfPosition]) {
+            newArray = arr.slice(halfPosition+1, arr.length);
+
+        } else if (num < arr[halfPosition]) {
+            newArray = arr.slice(0, halfPosition);
+        }
+
+        if(newArray.length) {
+            binarySearch(num, newArray)
+        }
+    }
+
+    binarySearch(70, array, []);
 }());
+
+
