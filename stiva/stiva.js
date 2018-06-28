09e3c4807476b1a086c0f60ca29b4ca5ae840caf
Mihai Munteanu <mum@ad.rms.intern>
2018-06-28 13:24:39 +0300

    rezolvare exercitiu Stiva

--- stiva/stiva.js
+++ stiva/stiva.js
@@ -1,23 +1,69 @@
-import { CustomEvents } from "./customEvents.js";
 
 export function Stack(size) {
     this._size = size;
-    this._stack = new Array(this._size);
+    this._stack = [];
 }
 
-Stack.prototype = Object.create(CustomEvents.prototype);
+Stack.prototype = {
 
-Object.assign(Stack.prototype, {
     constructor: Stack,
 
     push: function(item) {
-        this._stack.push(item)
+        if(this._stack.length <= this._size) {
+            this._stack.push(item);
+
+            return this._stack;
+        } else {
+            return this.is_full();
+        }
+
     },
 
     top: function() {
-        this._stack.slice(-1);
-    }
-});
+        if(this._stack.length >= 0) {
+            var topEl = this._stack.slice(-1);
 
-var x = new Stack(26);
-console.log(x._stack)
+            return topEl[0];
+        } else {
+            return this.is_empty();
+        }
+    },
+
+    pop: function() {
+        return this._stack.pop();
+    },
+
+    is_empty: function() {
+        if(!this._stack.length) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+
+    is_full: function() {
+        if(this._stack.length === this._size) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+
+    get_size: function() {
+        return this._stack.length;
+    },
+
+    clear: function() {
+        return this._stack = [];
+    }
+};
+
+var x = new Stack(15);
+console.log(x.push("test"));
+// console.log(x.top());
+// console.log(x.pop())
+// console.log(x.is_empty());
+// console.log(x.is_full());
+// console.log(x.get_size());
+// console.log(x.clear());
+// console.log(x)
